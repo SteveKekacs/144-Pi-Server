38e978b83270636c04e4e3d6b631b8d75978da75
Skekacs93 <stevenkekacs@college.harvard.edu>
2018-04-17 14:34:20 -0400

    Got remote video sending working

---
+++ video_sender.py
@@ -0,0 +1,47 @@
+"""
+Sends PiCamera video from Rasperry Pi to Remote Server
+for Object Detection Processing. On stop command,
+stops car.
+"""
+import cv2
+import numpy as np
+import socket
+import sys
+import pickle
+import struct
+
+# IP address of server to send video to
+HOST_IP = 'localhost'
+
+# Port to send over
+PORT = 8089
+
+
+def send_video():
+    # create socket
+    print("Creating socket...")
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket created...")
+
+    print("Connecting to %s:%d..." % (HOST_IP, PORT))
+    clientsocket.connect((HOST_IP, PORT))
+    print("Connected to %s:%d..." % (HOST_IP, PORT))
+
+    # capture video
+    print("Capturing video...")
+    cap=cv2.VideoCapture(0)
+
+    # continue to send video till interrupt
+    while True:
+        # read frame
+        ret, frame = cap.read()
+
+        # convert to string
+        data = pickle.dumps(frame)
+
+        # send data len then data to client
+        clientsocket.sendall(struct.pack("L", len(data)) + data)
+
+
+if __name__ == '__main__':
+    send_video()
