4d54a6f14e7d6bd0a9f24f8030cb1e81eaecab07
SteveKekacs <stevenkekacs@gmail.com>
2018-04-23 17:45:14 -0400

    cleaned up code + implemented different protocols with args

--- video_sender.py
+++ video_sender.py
@@ -16,17 +16,6 @@ from picamera import PiCamera
 # to speed things up, lower the resolution of the camera
 camera_width = 320
 camera_height = 240
-#scale_down = 6
-
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (camera_width, camera_height)
-camera.framerate = 32
-camera.rotation = 180
-rawCapture = PiRGBArray(camera, size=(camera_width, camera_height))
-
-# allow the camera to warmup
-time.sleep(0.1)
 
 # IP address of server to send video to
 # HOST_IP = '127.0.0.1'
@@ -36,40 +25,63 @@ HOST_IP = '10.251.46.150'
 PORT = 8089
 
 
-def send_video():
+def send_video(protocol):
     # create socket
-    print("Creating socket...")
-    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket created...")
+    # get socket type based on protocol
+    socket_type = socket.SOCK_STREAM
+    if protocol == 'UDP':
+        socket_type = socket.SOCK_DGRAM
 
-    print("Connecting to %s:%d..." % (HOST_IP, PORT))
-    clientsocket.connect((HOST_IP, PORT))
-    print("Connected to %s:%d..." % (HOST_IP, PORT))
+    print("Creating %s socket..." % protocol)
+    clientsocket = socket.socket(socket.AF_INET, socket_type)
+    print("%s Socket created..." % protocol)
 
-    # capture video
+    if protocol == 'TCP':
+        print("Connecting to %s:%d..." % (HOST_IP, PORT))
+        clientsocket.connect((HOST_IP, PORT))
+        print("Connected to %s:%d..." % (HOST_IP, PORT))
 
-    # continue to send video till interru
-    # capture frames from the camera
-    num = 1
+    # initialize the camera and grab a reference to the raw camera capture
+    print("Initializing Camera...")
+    camera = PiCamera()
+    camera.resolution = (camera_width, camera_height)
+    camera.framerate = 32
+    camera.rotation = 180
+    rawCapture = PiRGBArray(camera, size=(camera_width, camera_height))
+
+    # allow the camera to warmup
+    time.sleep(0.1)
+    print("Camera ready...")
+
+    # continue to capture frames from camera and
+    # send to remote server till interrupt
     for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        print('got img %d' % num)
         # grab the raw NumPy array representing the image, then initialize the timestamp
         # and occupied/unoccupied text
         frame = image.array
-        #frame = cv2.GaussianBlur(frame, (15,15), 0)
 
         # convert to string
         data = pickle.dumps(frame)
-        print('got data (%d) %d' % (len(data), num))
+
+        # pack message size into struct
         msg_size = struct.pack("<L", len(data))
-        print('packed struct %d' % num)
+
         # send data len then data to client
-        clientsocket.sendall(msg_size + data)
-        print('sent data %d' % num)
+        if protocol == 'TCP':
+            clientsocket.sendall(msg_size + data)
+        else:
+            clientsocket.sendto(msg_size + data, (HOST_IP, PORT))
+
         # clear the stream in preparation for the next frame
         rawCapture.truncate(0)
-        print('cleared capture %d\n\n' % num)
-        num += 1
+
 
 if __name__ == '__main__':
-    send_video()
+    protcol = 'TCP'
+    if sys.argv and sys.argv[0] in ['tcp', 'udp', 'rdp']:
+        protocol = sys.argv[0].upper()
+    else:
+        print("Error: Invalid argument")
+        return 
+
+    send_video(protocol)
